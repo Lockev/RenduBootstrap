eb328afc4a975a2775718f569ab5df77e36e361e
Guillaume PRIGENT <guillaume.prigent92@gmail.com>
2019-10-29 10:30:08 +0100

    scrollTo JS terminé

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".js-scrollTo").on("click", function() {
+    // Au clic sur un élément
+    var page = $(this).attr("href"); // Page cible
+    var speed = 750; // Durée de l'animation (en ms)
+    $("html, body").animate({ scrollTop: $(page).offset().top - 80 }, speed); // Go
+    return false;
+  });
+});
